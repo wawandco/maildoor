e6af769c121b71d4efe3b06384afa37242ec5fe1
Antonio Pagano <apagano@wawand.co>
2024-03-27 09:41:23 -0500

    sending email from the sample app

---
+++ internal/sample/auth.go
@@ -0,0 +1,82 @@
+package sample
+
+import (
+	"bytes"
+	_ "embed"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/smtp"
+	"os"
+	"text/template"
+
+	"github.com/wawandco/maildoor"
+)
+
+// Auth handler with custom email validator
+// and after login function
+var Auth = maildoor.New(
+	maildoor.UsePrefix("/auth/"),
+	maildoor.EmailValidator(validateEmail),
+	maildoor.AfterLogin(afterLogin),
+	maildoor.EmailSender(sendEmail),
+)
+
+// email struct to hold the email data to be used
+// with the email template
+type email struct {
+	From    string
+	To      string
+	Subject string
+	HTML    string
+	Text    string
+}
+
+//go:embed email_template.txt
+var mtmpl string
+
+// emailTmpl is the template to be used to send the email
+var emailTmpl = template.Must(template.New("email").Parse(mtmpl))
+
+// sendEmail function to send the multipart email to the user
+func sendEmail(to, html, txt string) error {
+	from := os.Getenv("SMTP_FROM")
+	password := os.Getenv("SMTP_PASS")
+	user := os.Getenv("SMTP_USER")
+
+	mb := bytes.NewBuffer([]byte{})
+	err := emailTmpl.Execute(mb, email{
+		HTML:    html,
+		Text:    txt,
+		From:    from,
+		To:      to,
+		Subject: "Your authentication code",
+	})
+
+	if err != nil {
+		return fmt.Errorf("error executing email template: %w", err)
+	}
+
+	auth := smtp.PlainAuth("", user, password, "smtp.resend.com")
+	err = smtp.SendMail("smtp.resend.com:587", auth, from, []string{to}, mb.Bytes())
+	if err != nil {
+		return fmt.Errorf("error sending smtp message: %w", err)
+	}
+
+	return nil
+}
+
+// afterLogin function to redirect the user to the private area
+func afterLogin(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/private", http.StatusFound)
+}
+
+// validateEmail function to validate the email address
+// in this case we are only allowing a@pagano.id
+func validateEmail(email string) error {
+	if email == "a@pagano.id" {
+		return nil
+	}
+
+	return errors.New("invalid email address")
+}
--- internal/sample/cmd/main.go
+++ internal/sample/cmd/main.go
@@ -1,63 +1,21 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log/slog"
 	"net/http"
-	"time"
 
-	"github.com/wawandco/maildoor"
 	"github.com/wawandco/maildoor/internal/sample"
-)
-
-// Auth handler with custom email validator
-// and after login function
-var auth = maildoor.New(
-	maildoor.UsePrefix("/auth/"),
-	maildoor.EmailValidator(func(email string) error {
-		if email == "a@pagano.id" {
-			return nil
-		}
-
-		return errors.New("invalid email address")
-	}),
-
-	maildoor.AfterLogin(func(w http.ResponseWriter, r *http.Request) {
-		expire := time.Now().AddDate(0, 0, 1)
-		cookie := http.Cookie{
-			Name:       "sample",
-			Value:      r.Context().Value("email").(string),
-			Path:       "/",
-			Domain:     r.Host,
-			Expires:    expire,
-			RawExpires: expire.Format(time.UnixDate),
-			MaxAge:     86400,
-			HttpOnly:   true,
-			SameSite:   http.SameSiteStrictMode,
-		}
-
-		http.SetCookie(w, &cookie)
-		http.Redirect(w, r, "/private", http.StatusFound)
-	}),
-
-	maildoor.EmailSender(func(to, html, txt string) error {
-		slog.Info("Sending email", "TO", to)
-		fmt.Println(txt)
-
-		return nil
-	}),
 )
 
 func main() {
 	r := http.NewServeMux()
 
 	// Auth handlers
-	r.Handle("/auth/", auth)
+	r.Handle("/auth/", sample.Auth)
 
 	// Application handlers
 	r.HandleFunc("/private", secure(sample.Private))
-	r.HandleFunc("/", sample.Home)
+	r.HandleFunc("/{$}", sample.Home)
 
 	slog.Info("Server running on :3000")
 	http.ListenAndServe(":3000", r)
