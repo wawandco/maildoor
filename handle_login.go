bac5e8b86adda1a431a6000a94715ba3c073c823
Antonio Pagano <apagano@wawand.co>
2024-03-31 13:31:18 -0500

    tweak: file naming adjustments

--- handle_login.go
+++ handle_login.go
@@ -6,6 +6,15 @@ import (
 
 // handleLogin enpoint renders the handleLogin page to enter the user
 // identifier.
-func (m *maildoor) handleLogout(w http.ResponseWriter, r *http.Request) {
-	m.logout(w, r)
+func (m *maildoor) handleLogin(w http.ResponseWriter, r *http.Request) {
+	data := atempt{
+		Logo: m.logoURL,
+	}
+
+	err := m.render(w, data, "layout.html", "handle_login.html")
+	if err != nil {
+		m.httpError(w, err)
+
+		return
+	}
 }
